2a425173e214acda0c06138b4e33b95e0ba47044
miamakela <n3mami00@students.oamk.fi>
2024-12-01 19:01:53 +0200

    Updated json, defined variables js, small changes to html and css

---
+++ js/mia/mia.js
@@ -0,0 +1,9 @@
+let points = document.querySelector('#points span')
+let imageDiv = document.querySelector('#imageArea')
+let image = document.querySelector('#imageArea img')
+let answers = document.querySelector('#answerArea ul')
+let answerList = document.querySelectorAll('#answerArea ul li')
+let tip = document.getElementById("tip")
+let funFact = document.getElementById("funFact")
+
+let currentIndex = 0

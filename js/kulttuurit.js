55e0c3e39702f747f2ba05e3d6b5b3e6b4c74e73
anniina-a <anniina.anturamaki@gmail.com>
2024-12-05 21:31:53 +0200

    Added some relevant variables and methods to kulttuurit.js file.

--- js/kulttuurit.js
+++ js/kulttuurit.js
@@ -1,3 +1,25 @@
-const canvas = document.querySelector('#gameCanvas')
 
+// Canvas
+const canvas = document.querySelector('#gameCanvas')
 const ctx = canvas.getContext('2d');
+
+//Parien valinta
+
+const games = {
+    images: ["Italia.png", "Ranska.png", "Espanja.png", "Saksa.png"],
+    countries: ["Italia", "Ranska", "Espanja", "Saksa"],
+    answers: ["Italia", "Ranska", "Espanja", "Saksa"],
+}
+
+//Muuttujat
+let currentGame = 0; //Pidetään kirjaa, mikä peli on käynnissä (0 ensimmäinen peli, 1 toinen jne.).
+let selectedPairs = []; //Pelaajan valitsemat yhdistelmät (kuva + maa).
+let score = 0; //Pelaajan kokonaispistemäärä.
+
+//Haetaan elementit
+const imagesContainer = document.getElementById('image-row');
+const countriesContainer = document.getElementById('country-row');
+const checkBtn = document.getElementById('check-button');
+const nextBtn = document.getElementById('next-button');
+const feedback = document.getElementById('feedback');
+const roundNumber = document.getElementById('game-round-row');

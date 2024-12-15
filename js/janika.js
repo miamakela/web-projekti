2bc6194543de0b8393056fea9a3a14a9126fa989
jasss-u <janika.lehtola@hotmail.com>
2024-12-15 12:35:11 +0200

    korjauksia

--- js/janika.js
+++ js/janika.js
@@ -1,71 +1,98 @@
 const cards = document.querySelectorAll(".card");
 
-let matchedCard = 0;
-let cardOne, cardTwo;
-let disableDeck = false;
+let matchedCard = 0; // Löydetyt parit
+let cardOne = null, cardTwo = null;
+let disableDeck = false; // Estää useamman kortin käännön samaan aikaan
+
+const pairsFound = document.getElementById("pairs-found"); // Laskuri
+
+const totalPairs = 8; // Pelin parimäärä
+
+function updateCounter() {
+    pairsFound.textContent = matchedCard; // Päivitä löydettyjen parien määrä
+}
 
 function flipCard(e) {
-    let clickedCard = e.target; //gettng user clicked card
-    if(clickedCard !== cardOne && !disableDeck) {
-        clickedCard.classList.add("flip");
-        if(!cardOne) { //return the cardOne value to clickedCard
-            return cardOne = clickedCard;
-        }
-        cardTwo = clickedCard;
-        disableDeck = true;
-        let cardOneImg = cardOne.querySelector("img").src,
-        cardTwoImg = cardTwo.querySelector("img").src;
-        matchCards (cardOneImg, cardTwoImg);
-
+    const clickedCard = e.target.closest(".card"); // Etsitään klikattu kortti
+    if (!clickedCard || clickedCard === cardOne || disableDeck || clickedCard.classList.contains("flip")) {
+        return; // Estetään virheellinen toiminta
     }
+
+    clickedCard.classList.add("flip"); // Käännä kortti
+
+    if (!cardOne) {
+        cardOne = clickedCard; // Ensimmäinen kortti
+        return;
+    }
+
+    cardTwo = clickedCard; // Toinen kortti
+    disableDeck = true; // Estetään lisää klikkauksia
+
+    const cardOneImg = cardOne.querySelector(".back-view img").src; // Ensimmäisen kortin kuva
+    const cardTwoImg = cardTwo.querySelector(".back-view img").src; // Toisen kortin kuva
+
+    matchCards(cardOneImg, cardTwoImg);
 }
 
 function matchCards(img1, img2) {
-    if(img1 === img2) { //if two cards img matched
-        matchedCard++; //increment matched value by 1
-        if(matchedCard == 8) {
-            return shuffleCard();
-        }
+    if (img1 === img2) {
+        // Jos kortit ovat pari
+        matchedCard++;
+        updateCounter(); // Päivitä laskuri
+
+        // Poistetaan kuuntelijat, jotta kortteja ei voi enää klikata
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
-        cardOne = cardTwo = ""; //setting both card value to blank
-        return disableDeck = false;
-    }
-    // if two cards not matched
-    setTimeout(() => {
-        //adding shake class to both cards after 400ms
-    cardOne.classList.add("shake");
-    cardTwo.classList.add("shake");
-    }, 400);
 
-    setTimeout(() => {
-        //adding shake class to both cards after 400ms
-    cardOne.classList.remove("shake", "flip");
-    cardTwo.classList.remove("shake", "flip");
-    cardOne = cardTwo = ""; //setting both card value to blank
-    disableDeck = false;
-    }, 1200);
+        resetCards();
+
+        if (matchedCard === totalPairs) {
+            setTimeout(shuffleCard, 1000); // Peli loppuu
+        }
+    } else {
+        // Kortit eivät ole pari, ravistetaan ja käännetään takaisin
+        setTimeout(() => {
+            cardOne.classList.remove("flip");
+            cardTwo.classList.remove("flip");
+            resetCards();
+        }, 1200);
+    }
+}
+
+function resetCards() {
+    cardOne = null;
+    cardTwo = null;
+    disableDeck = false; // Palautetaan klikkausmahdollisuus
 }
 
 function shuffleCard() {
-     matchedCard = 0;
-     cardOne, cardTwo = "";
-     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8,];
-     arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+    matchedCard = 0; // Nollaa löydetyt parit
+    updateCounter(); // Päivitä laskuri
+    cardOne = cardTwo = null;
 
-     cards.forEach((card, index) => {
-        card.classList.remove("flip");
-        let imgTag = card.querySelector("img");
-        imgTag.src = `../img/janika/img${arr[index]}.png`;
+    const arr = [...Array(totalPairs).keys(), ...Array(totalPairs).keys()]; // Kuvapareja varten
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1); // Satunnainen järjestys
+
+    cards.forEach((card, index) => {
+        card.classList.remove("flip"); // Käännä kaikki kortit takaisin
+        const frontImg = card.querySelector(".front-view img");
+        const backImg = card.querySelector(".back-view img");
+
+        // Etupuolen kuva pysyy "world.png"
+        frontImg.src = "../img/janika/world.png";
+
+        // Takapuolen kuva arvotaan
+        backImg.src = `../img/janika/img${arr[index]}.png`;
+
+        // Varmistetaan, että kortteja voi klikata
         card.addEventListener("click", flipCard);
-     });
-
+    });
 }
 
-shuffleCard();
-
-cards.forEach(card => { //click event to all cards
-    card.addEventListener("click", flipCard);
+shuffleCard(); // Käynnistä peli
+cards.forEach(card => {
+    card.addEventListener("click", flipCard); // Lisää klikkauskuuntelijat kortteihin
 });
+
 
 
